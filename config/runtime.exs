0521ab4007e661c3a50f2e245af92245b167bc4a
Callum McIntyre <callum.mcintyre@procensus.com>
2021-07-10 15:32:45 +0100

    Deploy the app to fly.io

---
+++ config/runtime.exs
@@ -0,0 +1,32 @@
+import Config
+
+if config_env() == :prod do
+  secret_key_base =
+    System.get_env("SECRET_KEY_BASE") ||
+      raise """
+      environment variable SECRET_KEY_BASE is missing.
+      You can generate one by calling: mix phx.gen.secret
+      """
+
+  app_name =
+    System.get_env("FLY_APP_NAME") ||
+      raise "FLY_APP_NAME not available"
+
+
+  config :callum_runs, CallumRunsWeb.Endpoint,
+    server: true,
+    url: [host: "#{app_name}.fly.dev", port: 80],
+    http: [
+      port: String.to_integer(System.get_env("PORT") || "4000"),
+      # IMPORTANT: support IPv6 addresses
+      transport_options: [socket_opts: [:inet6]]
+    ],
+    secret_key_base: secret_key_base
+end
+
+config :callum_runs, CallumRunsWeb.Endpoint,
+  graphjson_api_key: System.get_env("GRAPHJSON_API_KEY") ||
+    raise """
+    environment variable GRAPHJSON_API_KEY is missing.
+    This can be obtained from the graphjson.com dashboard
+    """
