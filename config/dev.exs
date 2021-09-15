c22a74e05bc7f8ad119e4fc825e096e91736a31d
Callum McIntyre <callum.mcintyre@procensus.com>
2021-09-15 21:31:49 +0100

    Bump graphjson project versions, remove 60s timeout

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :callum_runs, CallumRunsWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 4000, protocol_options: [idle_timeout: :infinity]],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
